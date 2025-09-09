08d4bfcdc35e0a7523d0718555bc9425a70b9d53
SiddabattuniGanesh <ganeshsiddabattuni@gmail.com>
2025-09-09 13:16:57 +0530

    Indentity Service Added

---
+++ identity-service/src/main/java/com/onevibe/identity_service/model/User.java
@@ -0,0 +1,28 @@
+package com.onevibe.identity_service.model;
+import com.onevibe.identity_service.model.User;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+}

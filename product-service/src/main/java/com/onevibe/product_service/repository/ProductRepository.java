23e0b75a0504817a2c387ab98c656a580f0bb324
SiddabattuniGanesh <ganeshsiddabattuni@gmail.com>
2025-09-09 14:48:19 +0530

    Product Service

---
+++ product-service/src/main/java/com/onevibe/product_service/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.onevibe.product_service.repository;
+
+import com.onevibe.product_service.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
